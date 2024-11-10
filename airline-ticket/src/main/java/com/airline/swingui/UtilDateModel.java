85e14c6bd40c256118f6fcf13b2b698cc34faeff
BuildTools <unconfigured@null.spigotmc.org>
2024-11-10 11:07:03 +0530

    Updated Flight Booking

---
+++ airline-ticket/src/main/java/com/airline/swingui/UtilDateModel.java
@@ -0,0 +1,15 @@
+package com.airline.swingui;
+
+public class UtilDateModel {
+
+    public void setSelected(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSelected'");
+    }
+
+    public void setDate(int i, int j, int k) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setDate'");
+    }
+
+}
